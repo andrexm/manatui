11cb34add6a62b560fd1fd6df04e80301183b238
André Nascimento <andre.neospace@gmail.com>
2026-06-02 22:45:47 -0300

    feat: button focus

--- examples/03buttons.c
+++ examples/03buttons.c
@@ -5,9 +5,13 @@
 const int START_Y = 3;
 const int START_X = 40;
 
+Application* app;
+Button* btn;
+Button* btn2;
 
-void btn_onclick() {
-  exit(0);
+void btn_onclick(int c) {
+  if (c == 10) container_print(app->focused_container, 1, 1, " Clicked!!!");
+  container_update((Container*)btn2, stdscr);
 }
 
 // move the cursor between each button after pressing TAB
@@ -15,41 +19,55 @@ void tab_buttons() {
   int y, x;
   getyx(stdscr, y, x);
   if (x == START_X || x == START_X + 1) {
-    move(START_Y + 1, START_X + 14 + 1);
+    wmove(btn->base.dwin, START_Y + 1, START_X + 14 + 1);
   } else {
-    move(START_Y + 1, START_X + 1);
+    wmove(btn2->base.dwin, START_Y + 1, START_X + 1);
   }
 }
 
 int main() {
   // Start the application
-  init_app();
+  app = init_app();
 
   // Create a container inside app
   Container* con = container_create();
 
   // Create a button inside app and initialize it
-  Button* btn = button_create(stdscr, 3, 12, START_Y, START_X, "click me", btn_onclick);
+  btn = button_create(stdscr, 3, 12, START_Y, START_X, "click me", btn_onclick);
   container_update((Container*)btn, stdscr);
 
   // Create another button
-  Button* btn2 = button_create(stdscr, 3, 14, START_Y, START_X + 14, "click me 2", btn_onclick);
+  btn2 = button_create(stdscr, 3, 14, START_Y, START_X + 14, "click me 2", btn_onclick);
   container_update((Container*)btn2, stdscr);
+
+  // Add both buttons to the focusable list
+  app_add_container(app, (Container*)btn);
+  app_add_container(app, (Container*)btn2);
 
   int c;
   while (c = getch()) {
+    // handle keys
     switch (c) {
       case 'q':
         goto finish;
         break;
 
       case '\t':
+        button_select(app, stdscr, (Container*)btn2);
         tab_buttons();
+        continue;
         break;
 
       default:
         break;
     }
+    // executes the on_focus method of the active container
+    if (app->focused_container != NULL) {
+
+      app->focused_container->on_focus(c);
+      continue;
+    }
+
   }
 
   finish:
