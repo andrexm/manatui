f7f7d6a63207a0f75f42a70cdd0543f8cbc53112
André Nascimento <andre.neospace@gmail.com>
2026-06-04 17:33:01 -0300

    feat: container title

--- examples/02container.c
+++ examples/02container.c
@@ -8,10 +8,9 @@ int main() {
   app_init();
 
   // Create a container inside app
-  Container* con = container_create(stdscr, 20, 20, 0, 1, TRUE, NULL);
+  Container* con = container_create(stdscr, 20, 20, 0, 1, NULL, TRUE, NULL);
 
   werase(con->dwin);
-  //box(con->dwin, 0, 0);
   container_print(con, 1, 1, "hello, welcome to my new library. It was written\ncompletely in %c and\n%s!", 'C', "ncurses");
   container_update(con, stdscr);
   getch();
