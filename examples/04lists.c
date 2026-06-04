3c535e4084dc3c21ec0c272282dcf09665ed4d20
André Nascimento <andre.neospace@gmail.com>
2026-06-04 20:41:58 -0300

    feat: default component actions and app key handling

--- examples/04lists.c
+++ examples/04lists.c
@@ -1,13 +1,17 @@
 #include "../src/ctui.h"
 #include <ncurses.h>
 
+Application* app = NULL;
+
 void list_focus(int c) {
-  //
+  Container* my_list = app->focused_container;
+  container_print(my_list, 1, 1, "content");
+  container_update(my_list, stdscr);
 }
 
 int main() {
   //start application
-  Application* app = app_init();
+  app = app_init();
 
   List* list = list_create(stdscr, 8, 20, 2, 2, "Languages", TRUE, list_focus);
   list_render(list);
@@ -18,10 +22,6 @@ int main() {
 
   int c;
   while ((c = getch())) {
-    if (c == 'q') break;
-    container_print((Container*)list, 1, 1, "content");
-    container_update((Container*)list, stdscr);
-
     // here magic happens!
     app_key_handle(app, c);
   }
