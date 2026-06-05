7d0e530614d55c0e24b15eaaf51f6d2a919bbae1
André Nascimento <andre.neospace@gmail.com>
2026-06-04 21:47:44 -0300

    refactor: consistent header-implementation structure

--- examples/01create.c
+++ examples/01create.c
@@ -1,13 +1,10 @@
-#include "../src/ctui.h"
+#include "../include/ctui.h"
 #include <ncurses.h>
-#include <stdio.h>
 
+// You can use default ncurses methods
 int main() {
-  // You can use default ncurses methods
+  Application* app = app_init();
 
-  Application* app;
-  // Start the application
-  app_init();
   mvprintw(1, 1, "height: %d", LINES);
   mvprintw(2, 1, "width: %d", COLS);
   refresh();
