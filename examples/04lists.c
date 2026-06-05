3a25418ec39e7dfe9beb416a2802bc3d9b8b34c1
André Nascimento <andre.neospace@gmail.com>
2026-06-05 09:27:14 -0300

    feat: add a single line to the list

--- examples/04lists.c
+++ examples/04lists.c
@@ -2,16 +2,20 @@
 
 void list_focus(int c, void* con) {
   Container* my_list = con;
-  container_print(my_list, 1, 1, "content");
-  container_update(my_list, stdscr);
+  //container_print(my_list, TRUE, 1, 1, "content");
+  //container_update(my_list, stdscr);
 }
 
 int main() {
   //start application
   Application* app = potatui_init();
 
-  List* list = list_create(stdscr, 8, 20, 2, 2, "Languages", TRUE, list_focus);
+  List* list = list_create(stdscr, 8, 50, 2, 2, "Languages", TRUE, list_focus);
   list->base.user_data = list; // add the list itself to the list_focus context
+
+  list_item_add(list, "this is the first line!");
+  list_item_add(list, "this is the second line!");
+  list_item_add(list, "this is the third line!");
   list_render(list);
 
   // set up list focus
