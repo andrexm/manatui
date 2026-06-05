4d15215d5138d0a30f61fb40368b8ebe99706ce1
André Nascimento <andre.neospace@gmail.com>
2026-06-05 12:48:07 -0300

    feat: add content to the list

--- examples/04lists.c
+++ examples/04lists.c
@@ -1,4 +1,5 @@
 #include "../include/potatui.h"
+#include <ncurses.h>
 
 void list_focus(int c, void* con) {
   Container* my_list = con;
@@ -13,10 +14,15 @@ int main() {
   List* list = list_create(stdscr, 8, 50, 2, 2, "Languages", TRUE, list_focus);
   list->base.user_data = list; // add the list itself to the list_focus context
 
-  list_item_add(list, "this is the first line!");
-  list_item_add(list, "this is the second line!");
-  list_item_add(list, "this is the third line!");
+  list_item_add(list, "%d this is the first line!", 1);
+  list_item_add(list, "%d this is the second line!", 2);
+  list_item_add(list, "%d this is the third line!", 3);
   list_render(list);
+
+  // we can read the list content whenever we want
+  for (int i = 0; i < list->items; i++) {
+    mvprintw(i + 1, 60, list->content[i]);
+  }
 
   // set up list focus
   app_add_container(app, (Container*)list);
