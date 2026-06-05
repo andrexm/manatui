ee3ec651391a32bf6f401795faf8b90f2cb1ade9
André Nascimento <andre.neospace@gmail.com>
2026-06-05 14:06:19 -0300

    feat: scroll the list

--- examples/04lists.c
+++ examples/04lists.c
@@ -14,9 +14,17 @@ int main() {
   List* list = list_create(stdscr, 8, 50, 2, 2, "Languages", TRUE, list_focus);
   list->base.user_data = list; // add the list itself to the list_focus context
 
-  list_item_add(list, "%d this is the first line!", 1);
-  list_item_add(list, "%d this is the second line!", 2);
-  list_item_add(list, "%d this is the third line!", 3);
+  list_item_add(list, "%d this is the 1 line!", 1);
+  list_item_add(list, "%d this is the 2 line!", 2);
+  list_item_add(list, "%d this is the 3 line!", 3);
+  list_item_add(list, "%d this is the 4 line!", 4);
+  list_item_add(list, "%d this is the 5 line!", 5);
+  list_item_add(list, "%d this is the 6 line!", 6);
+  list_item_add(list, "%d this is the 7 line!", 7);
+  list_item_add(list, "%d this is the 8 line!", 8);
+  list_item_add(list, "%d this is the 9 line!", 9);
+  list_item_add(list, "%d this is the 10 line!", 10);
+  list_item_add(list, "%d this is the 11 line!", 11);
   list_render(list);
 
   // we can read the list content whenever we want
