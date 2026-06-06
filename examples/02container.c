9c570de36c54d9859210aad8e6542771fa1d4bf7
André Nascimento <andre.neospace@gmail.com>
2026-06-05 21:51:01 -0300

    feat: describe each step

--- examples/02container.c
+++ examples/02container.c
@@ -5,14 +5,34 @@ int main() {
   // Start the application
   Application* app = potatui_init();
 
-  // Create a container inside app
-  Container* con = container_create(stdscr, 20, 20, 0, 1, NULL, TRUE, NULL);
+  int width = 30;
+  int height = 20;
+  int pos_x = (COLS / 2) - (width / 2); // we'll use this to draw the container at the center of the screen
 
-  werase(con->dwin);
-  container_print(con, 1, 1, "hello, welcome to my new library. It was written\ncompletely in %c and\n%s!", 'C', "ncurses");
-  container_update(con, stdscr);
-  getch();
+  // Create a container inside app
+  Container* con = // Container is the base for all components
+    container_create(
+      stdscr, // parent WINDOW*
+      height,
+      width,
+      1, // start at y row
+      pos_x, // start at x column
+      "Welcome", // title - you can pass NULL to avoid rendering the title
+      TRUE, // has border
+      NULL // function to handle focus
+    );
+
+  container_print(// this is how you put text inside a container - every other component uses it too
+                  con, // Container
+                  TRUE, // break line if the text is larger than the container width
+                  1, // start at y column of the container - 1 is to write below the top border
+                  1, // start at x row of the container - 1 writes after the left border
+                  "Hello, welcome to Potatui. It was written completely in %c and %s!", // we can pass a formatted string, like with printf
+                  'C', "ncurses" // then you pass as many parameters as you need in your string
+                );
+  container_update(con, stdscr); // this is a necessary step to draw the container correctly - you don't have to call it explicitly when using other components
+  getch(); // wait for a keypress
   
-  potatui_end();
+  potatui_end(); // finish the app
   return 0;
 }
