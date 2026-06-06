f5224eb3c17cfb4713d678c9014fdfc303844cbc
André Nascimento <andre.neospace@gmail.com>
2026-06-06 00:52:28 -0300

    feat: text input

---
+++ examples/05textinput.c
@@ -0,0 +1,19 @@
+#include "../include/potatui.h"
+#include <ncurses.h>
+
+int main() {
+  Application* app = potatui_init();
+
+  int width = 40;
+  int start_x = (COLS / 2) - (width / 2);
+  TextInput* input = textinput_create(stdscr, width, 1, start_x, "Your Name", NULL);
+
+  container_update((Container*)input, input->base.parent);
+  app_add_container(app, (Container*)input);
+  app->focused_container = (Container*)input;
+
+  potatui_loop(app);
+
+  potatui_end();
+  return 0;
+}
