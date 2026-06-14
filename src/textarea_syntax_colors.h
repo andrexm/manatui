f386d1448e5e4d12cc933f9d13ef294b49bb53c5
André Nascimento <andre.neospace@gmail.com>
2026-06-14 00:04:43 -0300

    feat: basic theme system

---
+++ src/textarea_syntax_colors.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <stdlib.h>
+
+// all colors should be hexadecimal
+typedef struct {
+  char* keyword_color;
+  char* function_color;
+  char* number_color; // for numbers inside the text
+  char* line_number_color; // the column of numbers on the textarea
+  char* symbol_color;
+  char* background_color;
+  char* background_variant_color;
+  char* comment_color;
+  char* string_color; // colorize inside double quotes
+  char* char_color; // colorize a single char inside single quotes
+  char* text_color; // other words, like variable names
+} TextareaTheme;
+
+const TextareaTheme default_theme = { "#009f80", "#5082be", "#cdaa19", "#76797a", "#b0b0b0", "#151517", "#303030", "#514f56", "#ff5822", "#f9a337", "#d6d6d6" };
+
+typedef struct {
+  const char** keywords;
+  int total;
+} LanguageData;
+
+static const char* c_keywords_list[] = {
+  "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else",
+  "enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short",
+  "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void",
+  "volatile", "while"
+};
+
+const LanguageData clang = {
+  c_keywords_list,
+  sizeof(c_keywords_list) / sizeof(char*)
+};
