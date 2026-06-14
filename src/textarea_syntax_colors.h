1cf2a1f324d45862aab7626d06645a90c5749582
André Nascimento <andre.neospace@gmail.com>
2026-06-14 20:57:03 -0300

    fix: add better colors for text

--- src/textarea_syntax_colors.h
+++ src/textarea_syntax_colors.h
@@ -17,7 +17,7 @@ typedef struct {
   char* text_color; // other words, like variable names
 } TextareaTheme;
 
-const TextareaTheme default_theme = { "#009f80", "#5082be", "#cdaa19", "#76797a", "#b0b0b0", "#151517", "#303030", "#514f56", "#ff5822", "#f9a337", "#d6d6d6" };
+const TextareaTheme default_theme = { "#009f80", "#5082be", "#cdaa19", "#76797a", "#999999", "#151517", "#303030", "#514f56", "#ff5822", "#f9a337", "#b2b2b2" };
 
 typedef struct {
   const char** keywords;
@@ -28,7 +28,7 @@ static const char* c_keywords_list[] = {
   "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else",
   "enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short",
   "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void",
-  "volatile", "while"
+  "volatile", "while", "include", "define", "pragma", "once"
 };
 
 const LanguageData clang = {
